d56ece4207c7fb6b5af84db80341a35af9c23ca3
Knika Dass <knikadass@gmail.com>
2020-07-14 22:46:10 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,13 @@ def line(deli)
   puts "The line is currently empty"
 else
   current_line = "The line is currently:"
-  deli.each_with_index(1) do |person, i|
+  deli.each_with_index(1) do |person,i|
     current_line << "#{i}. #{person}"
   end
-  pits
+  puts current_line
+end
+end
+
+def take_a_number(deli,name)
+  deli << name
+  
